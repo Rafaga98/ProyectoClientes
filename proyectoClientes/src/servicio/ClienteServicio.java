ddc762adb9f4581aa5a560dd003150063f52454e
Manuel Godoy <Manuel.Godoy008@gmail.com>
2022-08-05 22:02:19 -0400

    -creacion de atributo list en cliente servicio -generacion de constructor en cliente servicio -creacion de metodo retornoListaClientes

--- proyectoClientes/src/servicio/ClienteServicio.java
+++ proyectoClientes/src/servicio/ClienteServicio.java
@@ -1,5 +1,25 @@
 package servicio;
 
-public class ClienteServicio {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import modelo.Cliente;
 
+public class ClienteServicio {
+	List<Cliente> listaClientes = new ArrayList<>();
+
+	public ClienteServicio(List<Cliente> listaClientes) {
+		super();
+		this.listaClientes = listaClientes;
+	}
+
+	public void retornoListarClientes() {
+		Iterator<Cliente> nombreIterator = listaClientes.iterator();
+		while(nombreIterator.hasNext()){
+			Cliente cliente = nombreIterator.next();
+			System.out.println(cliente);
+		}
+	}
+	
+		
 }
