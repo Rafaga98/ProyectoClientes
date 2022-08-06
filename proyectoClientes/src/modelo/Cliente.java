8b17ae53941169bc1a58c8774595902c1d042991
Claudio Alarcon <54036219+Rafaga98@users.noreply.github.com>
2022-08-05 20:19:25 -0400

    CarpetaProyecto
    Carpeta con los archivos referentes al proyecto

---
+++ proyectoClientes/src/modelo/Cliente.java
@@ -0,0 +1,69 @@
+package modelo;
+
+public class Cliente {
+	String runCliente;
+	String nombreCliente;
+	String apellidoCliente;
+	String aniosCliente;
+	
+	
+	//constructor vacio
+	public Cliente() {
+		
+	}
+	
+	//constructor lleno
+	public Cliente(String runCliente, String nombreCliente, String apellidoCliente, String aniosCliente) {
+		//super();
+		this.runCliente = runCliente;
+		this.nombreCliente = nombreCliente;
+		this.apellidoCliente = apellidoCliente;
+		this.aniosCliente = aniosCliente;
+	}
+
+	//getters y setters
+	public String getRunCliente() {
+		return runCliente;
+	}
+
+	public void setRunCliente(String runCliente) {
+		this.runCliente = runCliente;
+	}
+
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
+
+	public void setNombreCliente(String nombreCliente) {
+		this.nombreCliente = nombreCliente;
+	}
+
+	public String getApellidoCliente() {
+		return apellidoCliente;
+	}
+
+	public void setApellidoCliente(String apellidoCliente) {
+		this.apellidoCliente = apellidoCliente;
+	}
+
+	public String getAniosCliente() {
+		return aniosCliente;
+	}
+
+	public void setAniosCliente(String aniosCliente) {
+		this.aniosCliente = aniosCliente;
+	}
+	
+	
+	//metodo toString
+	@Override
+	public String toString() {
+		return "Cliente [runCliente=" + runCliente + ", nombreCliente=" + nombreCliente + ", apellidoCliente="
+				+ apellidoCliente + ", aniosCliente=" + aniosCliente + "]";
+	}
+	
+	
+	
+	
+	
+}
